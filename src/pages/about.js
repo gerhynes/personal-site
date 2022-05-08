ac38483dbe1dddb42f7f785efa502cadca96ed5e
Gerard Hynes <gkhynes@gmail.com>
2022-05-08 21:14:27 +0100

    Fix bio image centering issue

--- src/pages/about.js
+++ src/pages/about.js
@@ -37,12 +37,12 @@ const BioStyles = styled.div`
 `;
 
 const BioImageWrapperStyles = styled.div`
-  text-align: center;
+  display: grid;
+  justify-items: center;
   margin-bottom: 1.5rem;
 `;
 
 const BioImageStyles = styled(GatsbyImage)`
-  margin-right: 1.5rem;
   border-radius: 50%;
   box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
     0 4px 6px -2px rgba(0, 0, 0, 0.05);
