2b06e209d88ba290d0afe4d2b608eb18a1a71f87
Gerard Hynes <gkhynes@gmail.com>
2022-12-11 13:34:18 +0000

    Add MDX utilities

---
+++ lib/mdxUtils.ts
@@ -0,0 +1,100 @@
+import fs from "fs";
+import { join } from "path";
+import matter from "gray-matter";
+
+export type Post = {
+  content: string;
+  data: {
+    [key: string]: any;
+  };
+};
+
+export type Project = {
+  content: string;
+  data: {
+    [key: string]: any;
+  };
+};
+
+const postsDirectory = join(process.cwd(), "posts");
+const projectsDirectory = join(process.cwd(), "projects");
+
+export function getPostSlugs(): string[] {
+  return fs.readdirSync(postsDirectory);
+}
+
+export function getProjectSlugs(): string[] {
+  return fs.readdirSync(projectsDirectory);
+}
+
+export function getPostBySlug(slug: string): string {
+  const realSlug = slug.replace(/\.mdx$/, "");
+  const fullPath = join(postsDirectory, `${realSlug}.mdx`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  return fileContents;
+}
+
+export function getProjectBySlug(slug: string): string {
+  const realSlug = slug.replace(/\.mdx$/, "");
+  const fullPath = join(projectsDirectory, `${realSlug}.mdx`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  return fileContents;
+}
+
+export function getAllPostSlugs(): string[] {
+  const slugs = fs.readdirSync(postsDirectory);
+
+  const realSlugs = slugs.map((slug) => slug.replace(/\.mdx$/, ""));
+
+  return realSlugs;
+}
+
+export function getAllProjectSlugs(): string[] {
+  const slugs = fs.readdirSync(projectsDirectory);
+
+  const realSlugs = slugs.map((slug) => slug.replace(/\.mdx$/, ""));
+
+  return realSlugs;
+}
+
+export function getAllPosts(): Post[] {
+  const slugs = getAllPostSlugs();
+
+  const postFiles = slugs.map((slug) => getPostBySlug(slug));
+
+  const posts = postFiles.map((post) => {
+    const { content, data } = matter(post);
+    return {
+      content,
+      data,
+    };
+  });
+
+  const sortedPosts = posts.sort(
+    (post1, post2) => post1.data.date - post2.data.date
+  );
+
+  return sortedPosts;
+}
+
+export function getAllProjects() {
+  const slugs = getAllProjectSlugs();
+
+  const projectFiles = slugs.map((slug) => getProjectBySlug(slug));
+
+  const projects = projectFiles.map((project) => {
+    const { content, data } = matter(project);
+    return {
+      content,
+      data,
+    };
+  });
+
+  const sortedProjects = projects.sort((project1, project2) =>
+    project1.data.date > project2.data.date ? -1 : 1
+  );
+
+  return sortedProjects;
+}
