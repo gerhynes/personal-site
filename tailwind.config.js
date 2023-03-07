ba76f6ff63f337c6f266ebc61c9eaf251f4daa66
Gerard Hynes <gkhynes@gmail.com>
2023-03-07 22:54:31 +0000

    Clean up Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,10 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   darkMode: "class",
-  purge: {
-    enabled: true,
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-  },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -18,9 +15,6 @@ module.exports = {
         serif: ["Source Code Pro", ...defaultTheme.fontFamily.serif],
       },
     },
-  },
-  variants: {
-    typography: ["dark"],
   },
   plugins: [require("@tailwindcss/typography")],
 };
