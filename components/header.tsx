015b099506d8c84fcb0f3064ced7e8f51ba264a7
Gerard Hynes <gkhynes@gmail.com>
2023-03-07 22:54:04 +0000

    Style dark mode toggle

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import { useTheme } from "next-themes";
 function Header() {
   const { theme, setTheme } = useTheme();
   return (
-    <header className="mx-auto flex max-w-3xl flex-wrap justify-between px-4 py-4 font-serif text-xl text-slate-900 dark:text-white">
+    <header className="mx-auto flex max-w-3xl flex-wrap items-center justify-between px-4 py-4 font-serif text-xl text-slate-900 dark:text-white">
       <h1 className="font-bold">
         <Link href="/">
           <a className="hover:underline">Gerard Hynes</a>
@@ -27,8 +27,46 @@ function Header() {
           </Link>
         </span>
       </div>
-      <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-        toggle
+      <button
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        className="rounded-full bg-slate-200 p-2 dark:bg-slate-700"
+      >
+        {theme === "dark" ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6"
+            aria-hidden="true"
+            focusable="false"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6"
+            aria-hidden="true"
+            focusable="false"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+            />
+          </svg>
+        )}
+        <span className="sr-only">Dark Mode toggle</span>
       </button>
     </header>
   );
