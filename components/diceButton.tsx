c6b33b147be2e9844010b3f4cc9806c53c50333f
Gerard Hynes <gkhynes@gmail.com>
2023-03-08 20:44:44 +0000

    Adjust dice roller sizes

--- components/diceButton.tsx
+++ components/diceButton.tsx
@@ -21,13 +21,14 @@ function DiceButton({
 
   return (
     <div className="group pointer-events-auto relative flex items-center">
-      <div className="relative h-16 w-16 rounded-full">
+      <div className="relative h-14 w-14 rounded-full">
         <button
-          className="flex h-16 w-16 items-center justify-center rounded-full bg-slate-700 group-hover:bg-slate-600"
+          className="flex h-14 w-14 items-center justify-center rounded-full bg-slate-700 group-hover:bg-slate-600"
           onClick={handleClick}
         >
           {die.image}
           {die.sides === 100 ? die.image : ""}
+          <span className="sr-only">{die.name}</span>
         </button>
         {die.count > 0 ? (
           <span className="absolute top-0 -right-2 flex w-6 items-center justify-center rounded-full bg-teal-500 text-white">
@@ -38,7 +39,7 @@ function DiceButton({
         )}
       </div>
       <div className="absolute inset-x-0 -bottom-4 flex justify-center text-slate-300">
-        <p className="block w-11 rounded-lg bg-slate-700 text-center group-hover:bg-slate-600">
+        <p className="block w-10 rounded-lg bg-slate-700 text-center text-sm group-hover:bg-slate-600">
           {die.name}
         </p>
       </div>
--- components/diceRoller.tsx
+++ components/diceRoller.tsx
@@ -19,43 +19,43 @@ function DiceRoller() {
     {
       name: "d4",
       sides: 4,
-      image: <D4 className="h-14 w-14 fill-slate-300" />,
+      image: <D4 className="h-12 w-12 fill-slate-300" />,
       count: 0,
     },
     {
       name: "d6",
       sides: 6,
-      image: <D6 className="h-14 w-14 fill-slate-300" />,
+      image: <D6 className="h-12 w-12 fill-slate-300" />,
       count: 0,
     },
     {
       name: "d8",
       sides: 8,
-      image: <D8 className="h-14 w-14 fill-slate-300" />,
+      image: <D8 className="h-12 w-12 fill-slate-300" />,
       count: 0,
     },
     {
       name: "d100",
       sides: 100,
-      image: <D10 className="h-14 w-14 fill-slate-300" />,
+      image: <D10 className="h-12 w-12 fill-slate-300" />,
       count: 0,
     },
     {
       name: "d10",
       sides: 10,
-      image: <D10 className="h-14 w-14 fill-slate-300" />,
+      image: <D10 className="h-12 w-12 fill-slate-300" />,
       count: 0,
     },
     {
       name: "d12",
       sides: 12,
-      image: <D12 className="h-14 w-14 fill-slate-300" />,
+      image: <D12 className="h-12 w-12 fill-slate-300" />,
       count: 0,
     },
     {
       name: "d20",
       sides: 20,
-      image: <D20 className="h-14 w-14 fill-slate-300" />,
+      image: <D20 className="h-12 w-12 fill-slate-300" />,
       count: 0,
     },
   ];
@@ -133,25 +133,27 @@ function DiceRoller() {
             />
           ))}
         </div>
-        <div className="pointer-events-auto flex gap-2" id="diceButtons">
+        <div className="pointer-events-auto flex gap-4" id="diceButtons">
           {menuIsOpen ? (
             <button
-              className="flex h-16 w-16 items-center justify-center rounded-full border-4 border-teal-500 bg-slate-600 hover:bg-slate-700"
+              className="flex h-14 w-14 items-center justify-center rounded-full border-4 border-teal-500 bg-slate-600 hover:bg-slate-700"
               onClick={clearDice}
             >
               <BsXLg className="h-6 w-6 fill-white" />
+              <span className="sr-only">Dice Menu</span>
             </button>
           ) : (
             <button
-              className="flex h-16 w-16 justify-center rounded-full bg-teal-500 hover:bg-teal-600"
+              className="flex h-14 w-14 items-center justify-center rounded-full bg-teal-500 hover:bg-teal-600"
               onClick={openMenu}
             >
-              <D20 className="h-16 w-16 fill-white" />
+              <D20 className="h-12 w-12 fill-white" />
+              <span className="sr-only">Dice Menu</span>
             </button>
           )}
           {diceToRoll.length > 0 ? (
             <button
-              className="h-16 w-24 rounded-full bg-teal-500 text-lg font-bold uppercase tracking-wider text-white hover:bg-teal-600"
+              className="h-14 w-24 rounded-full bg-teal-500 text-lg font-bold uppercase tracking-wider text-white hover:bg-teal-600"
               onClick={handleRoll}
             >
               Roll
