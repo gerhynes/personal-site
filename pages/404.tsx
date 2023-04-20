e7f48bbbc2a1112b68d858c10806c60d6208b5ab
Gerard Hynes <gkhynes@gmail.com>
2023-04-20 20:46:32 +0100

    Fix 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,21 +4,21 @@ export default function Custom404() {
     <main>
       <div className="mt-10">
         <h1 className="mb-12 text-center font-serif text-4xl font-bold text-slate-900 dark:text-white sm:text-5xl">
-          So it looks like that page doesn't exist
+          So it looks like that page doesn&apos;t exist
         </h1>
         <div className="flex flex-col items-center justify-center">
           <p className="mb-6 inline-block text-xl leading-7 text-slate-700 dark:text-slate-300">
             Were you looking for?
           </p>
-          <ul className="mb-6 text-lg leading-7 text-slate-700 dark:text-slate-300">
+          <ul className="mb-6 text-center text-lg leading-7 text-slate-700 dark:text-slate-300">
             <li className="mb-4 text-teal-700 underline dark:text-teal-300">
               <Link href="/about">Information about me</Link>
             </li>
             <li className="mb-4 text-teal-700 underline dark:text-teal-300">
-              <Link href="/projects">Personal projects I've built</Link>
+              <Link href="/projects">Personal projects I&apos;ve built</Link>
             </li>
             <li className="text-teal-700 underline dark:text-teal-300">
-              <Link href="/writing">Articles I've written</Link>
+              <Link href="/writing">Articles I&apos;ve written</Link>
             </li>
           </ul>
         </div>
