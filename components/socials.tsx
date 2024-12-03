7be848a555a14ff7114e3d733c8197e7a9f7ecb9
gerhynes <gkhynes@gmail.com>
2024-12-03 20:31:23 +0000

    Add Bluesky link

--- components/socials.tsx
+++ components/socials.tsx
@@ -1,12 +1,22 @@
-import { FaGithub, FaLinkedin, FaMastodon } from "react-icons/fa";
+import { FaBluesky, FaGithub, FaLinkedin, FaMastodon } from "react-icons/fa6";
 
 function Socials() {
   return (
     <div className="flex gap-6">
       <a
+        className="group -m-1 p-1"
+        aria-label="Bluesky"
+        href="https://bsky.app/profile/gerardhynes.com"
+      >
+        <FaBluesky
+          className="h-6 w-6 fill-teal-500 transition group-hover:fill-teal-600"
+          aria-hidden="true"
+        />
+      </a>
+      <a
         rel="me"
         className="group -m-1 p-1"
-        aria-label="LinkedIn"
+        aria-label="Mastodon"
         href="https://mastodon.ie/@gerhynes"
       >
         <FaMastodon
